b28961caca386506167e593701110c73f2e7f594
Nicolas Torion <nicolas.torion@bearstudio.fr>
2021-01-20 14:08:31 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView />
+    </>
+  );
+};
+
+export default App;
