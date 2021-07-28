a7bd8962612400b1a58111d03e8b79fe86a38610
qlerebours <quentin.lerebours@bearstudio.fr>
2021-07-28 17:53:54 +0200

    fix: export constants for colors and reuse them

--- constants/themes.js
+++ constants/themes.js
@@ -1,5 +1,12 @@
-export const primaryColor = '#0049C2';
-export const gray600Color = '#3E4B5D';
+import {
+  brandPrimaryColors,
+  errorColors,
+  grayColors,
+  warningColors,
+} from './colors';
+
+export const primaryColor = brandPrimaryColors.brandPrimary600;
+export const gray600Color = grayColors.gray600;
 export const bodyColor = '#F2F3F4';
 export const whiteColor = '#FFFFFF';
 
@@ -9,56 +16,15 @@ const THEMES = {
     colors: {
       body: bodyColor,
       text: gray600Color,
-      border: '#fff',
+      border: whiteColor,
       spinner: primaryColor,
       button: primaryColor,
       buttonBody: primaryColor,
 
-      // You can use tools like https://smart-swatch.netlify.app/ to generate colors
-      // Primary colors
-      brandPrimary50: '#e0f4ff',
-      brandPrimary100: '#b8dcfa',
-      brandPrimary200: '#8ec4f1',
-      brandPrimary300: '#63ace8',
-      brandPrimary400: '#3994e0',
-      brandPrimary500: '#1f7bc6',
-      brandPrimary600: '#135f9b',
-      brandPrimary700: '#084470',
-      brandPrimary800: '#002946',
-      brandPrimary900: '#000f1d',
-      // Error Colors
-      error050: '#FFEFF0',
-      error100: '#FFE0E2',
-      error200: '#FDC7CD',
-      error300: '#FC9AA6',
-      error400: '#F9677A',
-      error500: '#F13955',
-      error600: '#DB1F41',
-      error700: '#B51636',
-      error800: '#941533',
-      error900: '#7C1530',
-      // Warning Colors
-      warning050: '#fff7ed',
-      warning100: '#ffedd5',
-      warning200: '#fed7aa',
-      warning300: '#fdba74',
-      warning400: '#fb923c',
-      warning500: '#f97316',
-      warning600: '#ea580c',
-      warning700: '#c2410c',
-      warning800: '#9a3412',
-      warning900: '#7c2d12',
-      // Grayscale Colors
-      gray050: '#F9F9F9',
-      gray100: '#EFF3F8',
-      gray200: '#DEE5ED',
-      gray300: '#C4CFDC',
-      gray400: '#8999AE',
-      gray500: '#59697F',
-      gray600: gray600Color,
-      gray700: '#2D394A',
-      gray800: '#1C2533',
-      gray900: '#111724',
+      ...brandPrimaryColors,
+      ...errorColors,
+      ...warningColors,
+      ...grayColors,
     },
     components: {
       Text: {
