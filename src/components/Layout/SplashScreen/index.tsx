7b92520576b76491cceaa80ebaf89b19e45ed1ea
qlerebours <quentin.lerebours@bearstudio.fr>
2021-10-06 14:33:30 +0200

    Fix unnecessary fragment

--- src/components/Layout/SplashScreen/index.tsx
+++ src/components/Layout/SplashScreen/index.tsx
@@ -9,17 +9,15 @@ const SplashScreen = () => {
   const theme = useTheme();
 
   return (
-    <>
-      <Box h="100%" p={5} justifyContent="center" alignItems="center">
-        <VStack justifyContent="center" space="xl">
-          <Logo maxWidth="100%" />
-          <Text fontSize="2xl" textAlign="center">
-            Loading...
-          </Text>
-          <ActivityIndicator color={theme.colors.primary['500']} size="large" />
-        </VStack>
-      </Box>
-    </>
+    <Box h="100%" p={5} justifyContent="center" alignItems="center">
+      <VStack justifyContent="center" space="xl">
+        <Logo maxWidth="100%" />
+        <Text fontSize="2xl" textAlign="center">
+          Loading...
+        </Text>
+        <ActivityIndicator color={theme.colors.primary['500']} size="large" />
+      </VStack>
+    </Box>
   );
 };
 
