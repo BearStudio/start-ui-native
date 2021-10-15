6564c6d41e141d87601fe05763a35807ff28d855
Ivan Dalmet <ivan@dalmet.fr>
2021-10-15 13:15:17 +0200

    fix: Test

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,10 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['./src/tests/setup.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '.+\\.(png|jpg|jpeg|ttf|woff|woff2)$': 'jest-transform-stub',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-native|@react-navigation)',
+  ],
 };
