aca6de898c73b24a05be83d574ff4e2d2420bff1
Nicolas Torion <nicolas.torion@bearstudio.fr>
2024-01-31 12:39:32 +0100

    Update to expo 50

--- babel.config.js
+++ babel.config.js
@@ -4,20 +4,9 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
-      require.resolve('expo-router/babel'),
       ['module:react-native-dotenv'],
       ['transform-inline-environment-variables'],
       'react-native-reanimated/plugin',
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@': './src',
-          },
-          extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        },
-      ],
     ],
   };
 };
