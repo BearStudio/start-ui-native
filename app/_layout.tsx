3e80b5c38e305cb56f163c27f83dfa2707114e1e
Nicolas Torion <nicolas.torion@bearstudio.fr>
2024-02-03 20:09:38 +0100

    Remove useless index file for expo router

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Slot } from 'expo-router';
-import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import { ThemeProvider } from 'react-native-ficus-ui';
 
 import { queryClient } from '@/api/query-client';
 import useProtectedRoute from '@/modules/auth/auth.hook';
 import theme from '@/theme';
 
-const App = () => {
+export default function RootLayout() {
   useProtectedRoute();
 
   return (
@@ -19,8 +18,4 @@ const App = () => {
       </ThemeProvider>
     </QueryClientProvider>
   );
-};
-
-registerRootComponent(App);
-
-export default App;
+}
