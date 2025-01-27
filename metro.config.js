cdef55aa2763e87f1e3e0b4a19834d4fea1ba779
Hugo Pérard <hugo.perard@bearstudio.fr>
2025-01-27 12:02:27 +0100

    update storybook version

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,21 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-// eslint-disable-next-line
-const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path'); // eslint-disable-line @typescript-eslint/no-var-requires
+const { getDefaultConfig } = require('expo/metro-config'); // eslint-disable-line @typescript-eslint/no-var-requires
+const withStorybook = require('@storybook/react-native/metro/withStorybook'); // eslint-disable-line @typescript-eslint/no-var-requires
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+module.exports = withStorybook(config, {
+  // Set to false to remove storybook specific options
+  // you can also use a env variable to set this
+  enabled: true,
+  // Path to your storybook config
+  configPath: path.resolve(__dirname, './.storybook'),
+
+  // Optional websockets configuration
+  // Starts a websocket server on the specified port and host on metro start
+  // websockets: {
+  //   port: 7007,
+  //   host: 'localhost',
+  // },
+});
