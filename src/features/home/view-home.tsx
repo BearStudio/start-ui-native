c0c1a9321ec3f95b3ad29de5af49884c2cdd2978
Hugo Pérard <hugo.perard@bearstudio.fr>
2025-10-15 14:33:38 +0200

    clean marketing

--- src/features/home/view-home.tsx
+++ src/features/home/view-home.tsx
@@ -1,94 +1,11 @@
 import appConfig from 'app.config';
 import { Link } from 'expo-router';
 import { useTranslation } from 'react-i18next';
-import { Linking, TouchableOpacity } from 'react-native';
-import {
-  Box,
-  Button,
-  Center,
-  HStack,
-  Image,
-  ScrollBox,
-  Stack,
-  Text,
-  VStack,
-} from 'react-native-ficus-ui';
+import { Button, HStack, ScrollBox, Stack, Text } from 'react-native-ficus-ui';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import { MarketingBento } from '@/features/marketing/marketing-bento';
 import { ViewTabContent } from '@/layout/view-tab-content';
-
-const Tile = ({
-  href,
-  uri,
-  aspectRatio,
-}: {
-  href: string;
-  uri: string;
-  aspectRatio: number;
-}) => {
-  const handleOpenUrl = async (url: string) => {
-    const canOpen = await Linking.canOpenURL(url);
-    if (canOpen) {
-      Linking.openURL(url);
-    }
-  };
-  return (
-    <TouchableOpacity
-      onPress={() => handleOpenUrl(href)}
-      accessibilityRole="link"
-      style={{
-        aspectRatio,
-        width: '100%',
-        borderRadius: 8,
-        overflow: 'hidden',
-        backgroundColor: 'rgba(0,0,0,0.2)',
-      }}
-    >
-      <Image source={{ uri }} w="100%" h="100%" resizeMode="cover" />
-    </TouchableOpacity>
-  );
-};
-
-const MarketingLinks = () => (
-  <VStack spacing={8} mt="2xl">
-    <Box flexDirection="row" gap={8}>
-      <Box flex={1} gap={8}>
-        <Tile
-          href="https://bear.studio/assets-start-ui-bento-01"
-          uri="https://raw.githubusercontent.com/BearStudio/assets/main/start-ui/marketing-bento-01.jpg"
-          aspectRatio={0.7}
-        />
-
-        <Tile
-          href="https://github.com/BearStudio/start-ui-web"
-          uri="https://start-ui.com/_next/image?url=%2Fweb.jpg&w=3840&q=75"
-          aspectRatio={1.45}
-        />
-      </Box>
-
-      <Box flex={1} gap={8}>
-        <Tile
-          href="https://bear.studio/assets-start-ui-bento-04"
-          uri="https://raw.githubusercontent.com/BearStudio/assets/main/start-ui/marketing-bento-04.jpg"
-          aspectRatio={1.45}
-        />
-
-        <Tile
-          href="https://bear.studio/assets-start-ui-bento-03"
-          uri="https://raw.githubusercontent.com/BearStudio/assets/main/start-ui/marketing-bento-03.jpg"
-          aspectRatio={0.7}
-        />
-      </Box>
-    </Box>
-    <Box>
-      <Tile
-        href="https://ficus-ui.com/"
-        uri="https://raw.githubusercontent.com/BearStudio/react-native-ficus-ui/refs/heads/main/apps/docs/static/img/banner.png"
-        aspectRatio={2}
-      />
-    </Box>
-  </VStack>
-);
 
 export const ViewHome = () => {
   const { t } = useTranslation(['home']);
@@ -115,17 +32,7 @@ export const ViewHome = () => {
             </Button>
           </Link>
         </HStack>
-        <MarketingLinks />
-        <Center mt="xl" mb={100}>
-          <Text
-            textAlign="center"
-            fontSize="xs"
-            color="neutral.600"
-            _dark={{ color: 'neutral.300' }}
-          >
-            {t('welcome.caption')}
-          </Text>
-        </Center>
+        <MarketingBento />
       </ScrollBox>
     </ViewTabContent>
   );
