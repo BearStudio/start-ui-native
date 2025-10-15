23e2a9dc2feb72061da460b1951472e44f1a4b36
Hugo Pérard <hugo.perard@bearstudio.fr>
2025-10-15 19:23:50 +0200

    with fix layout

--- src/features/home/view-home.tsx
+++ src/features/home/view-home.tsx
@@ -1,8 +1,7 @@
 import appConfig from 'app.config';
 import { Link } from 'expo-router';
 import { useTranslation } from 'react-i18next';
-import { Button, HStack, ScrollBox, Stack, Text } from 'react-native-ficus-ui';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Button, HStack, Stack, Text } from 'react-native-ficus-ui';
 
 import { useShare } from '@/hooks/use-share';
 
@@ -13,47 +12,44 @@ import { ViewTabContent } from '@/layout/view-tab-content';
 
 export const ViewHome = () => {
   const { t } = useTranslation(['home']);
-  const insets = useSafeAreaInsets();
 
   const share = useShare();
 
   return (
-    <ViewTabContent mt={insets.top} gap={16}>
-      <ScrollBox flex={1}>
-        <Stack gap={8}>
-          <Text fontWeight="bold" fontSize="xl">
-            {t('home:welcome.title')}
-          </Text>
-          <Text fontWeight="medium">{t('home:welcome.description')}</Text>
-        </Stack>
-        <HStack gap={8} mt="md">
-          <Link href={appConfig.githubUrl} asChild>
-            <Button size="sm" variant="@secondary">
-              GitHub
-            </Button>
-          </Link>
-          <Link href={`${appConfig.githubUrl}/issues/new`} asChild>
-            <Button size="sm" variant="@secondary">
-              {t('home:welcome.openIssue')}
-            </Button>
-          </Link>
-          <Button
-            size="sm"
-            variant="@secondary"
-            onPress={() =>
-              share.open({
-                title: 'Github • Start UI [native]',
-                message: appConfig.githubUrl,
-              })
-            }
-            isLoading={share.isPending}
-          >
-            <IconShare2 width={16} height={16} />
-            {t('home:welcome.share')}
+    <ViewTabContent gap={16}>
+      <Stack gap={8}>
+        <Text fontWeight="bold" fontSize="xl">
+          {t('home:welcome.title')}
+        </Text>
+        <Text fontWeight="medium">{t('home:welcome.description')}</Text>
+      </Stack>
+      <HStack gap={8} mt="md">
+        <Link href={appConfig.githubUrl} asChild>
+          <Button size="sm" variant="@secondary">
+            GitHub
           </Button>
-        </HStack>
-        <MarketingBento />
-      </ScrollBox>
+        </Link>
+        <Link href={`${appConfig.githubUrl}/issues/new`} asChild>
+          <Button size="sm" variant="@secondary">
+            {t('home:welcome.openIssue')}
+          </Button>
+        </Link>
+        <Button
+          size="sm"
+          variant="@secondary"
+          onPress={() =>
+            share.open({
+              title: 'Github • Start UI [native]',
+              message: appConfig.githubUrl,
+            })
+          }
+          isLoading={share.isPending}
+        >
+          <IconShare2 width={16} height={16} />
+          {t('home:welcome.share')}
+        </Button>
+      </HStack>
+      <MarketingBento />
     </ViewTabContent>
   );
 };
--- src/layout/view-tab-content.tsx
+++ src/layout/view-tab-content.tsx
@@ -1,12 +1,25 @@
-import { Box, BoxProps } from 'react-native-ficus-ui';
+import { Box, BoxProps, ScrollBox } from 'react-native-ficus-ui';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
+import { WITH_NATIVE_TABS } from '@/app/(logged)/(tabs)/_layout';
+import { isApple } from '@/constants/device';
+
 export const ViewTabContent = ({
-  withHeader,
+  withHeader = isApple && WITH_NATIVE_TABS,
+  children,
   ...props
 }: BoxProps & { withHeader?: boolean }) => {
   const insets = useSafeAreaInsets();
+
   return (
-    <Box p={16} flex={1} mt={withHeader ? undefined : insets.top} {...props} />
+    <ScrollBox
+      p={16}
+      pt={(withHeader ? 0 : insets.top) + 16}
+      flex={1}
+      {...props}
+    >
+      {children}
+      <Box h={insets.bottom} />
+    </ScrollBox>
   );
 };
