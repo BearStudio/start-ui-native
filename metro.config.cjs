cfa1e277c6a8f550bae2b03a46d097ef1179b33b
AzizOuertaani <aziz@bearstudio.fr>
2025-10-16 12:26:00 +0200

    feat(storybook): setup and initial configuration of Storybook

---
+++ metro.config.cjs
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config');
+
+const withStorybook = require('@storybook/react-native/metro/withStorybook');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = withStorybook(config, {
+  enabled: true,
+  configPath: path.resolve(__dirname, './.rnstorybook'),
+});
