c45cd1f45e22fc81261afcf8e5a0c2a6b8e0946c
Hugo Pérard <hugo.perard@bearstudio.fr>
2025-10-17 15:18:36 +0200

    not found style and translation

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,16 +1,21 @@
 import { Link } from 'expo-router';
-import { Button, Center, Text } from 'react-native-ficus-ui';
+import { useTranslation } from 'react-i18next';
+import { Box, Button, Center, Text } from 'react-native-ficus-ui';
 
 import { ViewSafeContent } from '@/layout/view-safe-content';
 
 export default function () {
+  const { t } = useTranslation(['layout']);
+
   return (
     <ViewSafeContent>
-      <Center flex={1}>
-        <Text>This page doesn't exists 🤔</Text>
-        <Link href="/(logged)/(tabs)/home" asChild>
-          <Button>Go back in safe place</Button>
-        </Link>
+      <Center flex={1} gap={16}>
+        <Text fontSize="xl">{t('layout:notFound.title')}</Text>
+        <Box>
+          <Link href="/(logged)/(tabs)/home" asChild>
+            <Button>{t('layout:notFound.backInSafety')}</Button>
+          </Link>
+        </Box>
       </Center>
     </ViewSafeContent>
   );
