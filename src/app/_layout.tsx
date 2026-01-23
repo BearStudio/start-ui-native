01c8dc04ae1b6acbb5cbf7423382fd710e5251ab
Hugo Pérard <hugo.perard@bearstudio.fr>
2026-01-23 17:29:09 +0100

    working uniwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,16 +2,13 @@ import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Slot } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { FicusProvider } from 'react-native-ficus-ui';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import {
-  initialWindowMetrics,
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
+import { SafeAreaListener } from 'react-native-safe-area-context';
+import { Uniwind } from 'uniwind';
 import 'react-native-reanimated';
 import '@/lib/i18n';
 
-import theme from '@/lib/ficus-ui/theme';
+import '@/global.css';
 
 import { ThemeManager } from '@/components/theme-manager';
 import { Sonner } from '@/components/ui/sonner';
@@ -27,20 +24,22 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <FicusProvider theme={theme}>
-        <ThemeManager />
-        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-          <GestureHandlerRootView>
-            <BottomSheetModalProvider>
-              <SplashScreenManager>
-                <Slot />
-              </SplashScreenManager>
-              <Sonner />
-              {process.env.NODE_ENV === 'development' && <DevTools />}
-            </BottomSheetModalProvider>
-          </GestureHandlerRootView>
-        </SafeAreaProvider>
-      </FicusProvider>
+      <ThemeManager />
+      <SafeAreaListener
+        onChange={({ insets }) => {
+          Uniwind.updateInsets(insets);
+        }}
+      >
+        <GestureHandlerRootView>
+          <BottomSheetModalProvider>
+            <SplashScreenManager>
+              <Slot />
+            </SplashScreenManager>
+            <Sonner />
+            {process.env.NODE_ENV === 'development' && <DevTools />}
+          </BottomSheetModalProvider>
+        </GestureHandlerRootView>
+      </SafeAreaListener>
     </QueryClientProvider>
   );
 }
